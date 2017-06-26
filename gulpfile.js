ce6a89090e10bd0b3c9265153c21e1e0e9519fc0
Frank Scherer <frnksr@gmail.com>
2017-06-26 09:05:50 +0200

    automated browser update by gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect'); // Run a local dev server
 var open = require('gulp-open'); // open a url in a web browser
+var browserify = require('browserify'); // Bundles js files
+var reactify = require('reactify'); // Transforms React JSX to JS
+var source = require('viny-source-stream'); // Use conventional text streams with gulp
 
 // Default config
 var config = {
@@ -16,26 +19,31 @@ var config = {
 }
 
 // Start a local dev server
-gulp.task('connect', () => {
-    connect.server({
-        root: ['dist'],
-        port: config.port,
-        base: config.devBaseUrl,
-        livereload: true
-    });
+gulp.task('connect', function() {
+  connect.server({
+      root: config.paths.dist,
+      port: config.port,
+      livereload: true
+  });
 });
+
 
 // Opens the default document on the dev server
 gulp.task('open', ['connect'], () => {
-    gulp.src('dist/index.html')
-        .pipe(open({ url: config.devBaseUrl + ':' + config.port + '/' }));
+    gulp.src('./dist/index.html')
+        .pipe(open({ uri: config.devBaseUrl + ':' + config.port }));
 });
 
 // Copies any html files put to dist and reload web server
 gulp.task('html', () => {
-    gulp.src(config.paths.html)
+    return gulp.src(config.paths.html)
         .pipe(gulp.dest(config.paths.dist))
         .pipe(connect.reload());
 });
 
-gulp.task('default', ['html', 'open']);
+// Watches changes in the html folder
+gulp.task('watch', () => {
+    gulp.watch(config.paths.html, ['html']);
+});
+
+gulp.task('default', ['html', 'open', 'watch']);
